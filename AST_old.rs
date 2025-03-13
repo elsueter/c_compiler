e8df8749d27a5acd008f129597d8cc9e155c0a41
elsueter <joseph_straw@outlook.com>
2025-03-13 23:22:55 +0000

    moved old_AST to new file and started AFG work

---
+++ AST_old.rs
@@ -0,0 +1,56 @@
+#[derive(PartialEq)]
+enum Type {
+    Operator,
+    Numeric,
+}
+
+//utility struct to hold a 'token' along with an associated id
+pub struct Token {
+    pub id: Type,
+    pub val: Vec<u8>,
+}
+
+//taken an infix string and returns a vector of 'tokens' in postfix notation
+pub fn to_postfix(input: Vec<u8>) -> Vec<Token> {
+    let mut tokens: Vec<Token> = vec![];
+
+    let mut cur_type: Type = Type::Operator;
+    let mut cur_token: Vec<u8> = vec![];
+    for c in input {
+        match c {
+            42 | 43 | 45 | 47 => {
+                if cur_type != Type::Operator {
+                    // TODO restructure to remove this if on every iteration
+                    if !cur_token.is_empty() {
+                        tokens.push(Token {
+                            id: cur_type,
+                            val: cur_token,
+                        });
+                    }
+                    cur_type = Type::Operator;
+                    cur_token = vec![];
+                }
+                cur_token.push(c);
+            }
+            _ => {
+                if cur_type != Type::Numeric {
+                    // TODO restructure to remove this if on every iteration
+                    if !cur_token.is_empty() {
+                        tokens.push(Token {
+                            id: cur_type,
+                            val: cur_token,
+                        });
+                    }
+                    cur_type = Type::Numeric;
+                    cur_token = vec![];
+                }
+                cur_token.push(c);
+            }
+        }
+    }
+    tokens.push(Token {
+        id: cur_type,
+        val: cur_token,
+    });
+    tokens
+}
--- src/ast.rs
+++ src/ast.rs
@@ -1,56 +1,111 @@
-#[derive(PartialEq)]
-enum Type {
-    Operator,
-    Numeric,
+// Grammar
+//
+// Example Code:
+//  int main(argc, argv){
+//      int foo = 0;
+//      if(foo != 10){
+//          return 0;
+//      }
+//      return 1;
+//  }
+//
+// type_specifieer
+//  : int
+//  | void
+//  | ... TODO
+//
+//  IDENTIFIER
+//  : [A-Za-z0-9_.]*
+//
+// assignment_operator
+//  : =
+//
+// arithmetic_operator
+//  : +
+//  | -
+//  | ... TODO
+//
+// prefix_operator
+//  : +
+//  | -
+//  | ++
+//  | ... TODO
+//
+// postfix_operator
+//  : +
+//  | -
+//  | ++
+//  | --
+//  | ... TODO
+//
+// operator
+//  : arithmetic_operator
+//  | ... TODO
+//
+// expression
+//  : identifier
+//  | expression operator expression
+//  | prefix_operator expression
+//  | expression postfix_operator
+//
+// declaration
+//  : type_specifier IDENTIFIER assignment_operator expression
+//
+// body
+//  : { expression* }
+//
+// conditional_statement
+//  : if ( expression ) body
+//
+// return_statement
+//  : return expression
+//
+enum TypeSpecifier {
+    Void,
+    Int,
 }
 
-//utility struct to hold a 'token' along with an associated id
-pub struct Token {
-    pub id: Type,
-    pub val: Vec<u8>,
+enum AssignmentOperator {
+    Equals,
 }
 
-//taken an infix string and returns a vector of 'tokens' in postfix notation
-pub fn to_postfix(input: Vec<u8>) -> Vec<Token> {
-    let mut tokens: Vec<Token> = vec![];
+enum ArithmeticOperator {
+    Plus,
+    Minus,
+}
 
-    let mut cur_type: Type = Type::Operator;
-    let mut cur_token: Vec<u8> = vec![];
-    for c in input {
-        match c {
-            42 | 43 | 45 | 47 => {
-                if cur_type != Type::Operator {
-                    // TODO restructure to remove this if on every iteration
-                    if cur_token.len() > 0 {
-                        tokens.push(Token {
-                            id: cur_type,
-                            val: cur_token,
-                        });
-                    }
-                    cur_type = Type::Operator;
-                    cur_token = vec![];
-                }
-                cur_token.push(c);
-            }
-            _ => {
-                if cur_type != Type::Numeric {
-                    // TODO restructure to remove this if on every iteration
-                    if cur_token.len() > 0 {
-                        tokens.push(Token {
-                            id: cur_type,
-                            val: cur_token,
-                        });
-                    }
-                    cur_type = Type::Numeric;
-                    cur_token = vec![];
-                }
-                cur_token.push(c);
-            }
-        }
-    }
-    tokens.push(Token {
-        id: cur_type,
-        val: cur_token,
-    });
-    tokens
+enum PrefixOperator {
+    UnaryPrefixPlus,
+    UnaryPrefixMinus,
+}
+
+enum PostfixOperator {
+    UnaryPostfixPlus,
+    UnaryPostfixMinus,
+}
+
+enum Operator {
+    ArithmeticOperator,
+    PrefixOperator,
+    PostfixOperator,
+}
+
+enum Expression {
+    ID(String),
+}
+
+struct Declaration {
+    c_type: TypeSpecifier,
+    name: String,
+    assignment: AssignmentOperator,
+    rhs: Vec<Expression>,
+}
+
+fn test() {
+    let mine = Declaration {
+        c_type: TypeSpecifier::Void,
+        name: "test".to_string(),
+        assignment: AssignmentOperator::Equals,
+        rhs: vec![],
+    };
 }
