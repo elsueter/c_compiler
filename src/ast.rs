047ac4abc5406c73daf70e536ebbb470048ebdc9
elsueter <joseph_straw@outlook.com>
2025-03-16 23:15:19 +0000

    Implemented in-memory function structure

--- src/ast.rs
+++ src/ast.rs
@@ -90,22 +90,103 @@ enum Operator {
     PostfixOperator,
 }
 
-enum Expression {
-    ID(String),
+enum Parameter {
+    Identifier(String),
 }
 
-struct Declaration {
-    c_type: TypeSpecifier,
+enum ResolvableValue {
+    Value(i32),
+    Variable(String),
+    Expression(Expression),
+}
+
+//TODO - remove box usage, box is heap allocated and is very slow
+struct Expression {
+    lhs: Box<ResolvableValue>,
+    rhs: Option<RExpression>,
+}
+
+//TODO - remove box usage, box is heap allocated and is very slow
+struct RExpression {
+    op: Operator,
+    rhs: Box<ResolvableValue>,
+}
+
+impl Expression {
+    fn render(&self) -> String {
+        let mut return_string = String::from("");
+        match &(*self.lhs) {
+            ResolvableValue::Value(x) => return_string += &x.to_string(),
+            ResolvableValue::Variable(x) => return_string += x,
+            ResolvableValue::Expression(x) => return_string += &x.render(),
+        }
+
+        if let Some(rv) = &self.rhs {
+            return_string += match rv.op {
+                Operator::ArithmeticOperator => "+",
+                _ => "another_one",
+            };
+
+            match &(*rv.rhs) {
+                ResolvableValue::Value(x) => return_string += &x.to_string(),
+                ResolvableValue::Variable(x) => return_string += x,
+                ResolvableValue::Expression(x) => return_string += &x.render(),
+            }
+        }
+
+        return_string
+    }
+}
+
+struct Func {
+    identifier: TypeSpecifier,
     name: String,
-    assignment: AssignmentOperator,
-    rhs: Vec<Expression>,
+    parameters: Vec<Parameter>,
+    body: Vec<Expression>,
 }
 
-fn test() {
-    let mine = Declaration {
-        c_type: TypeSpecifier::Void,
-        name: "test".to_string(),
-        assignment: AssignmentOperator::Equals,
-        rhs: vec![],
+impl Func {
+    fn render(&self) -> String {
+        let mut return_string = String::from("");
+
+        match &self.identifier {
+            TypeSpecifier::Void => return_string.push_str("void"),
+            TypeSpecifier::Int => return_string.push_str("int"),
+        }
+
+        return_string += " ";
+
+        return_string.push_str(&self.name);
+
+        return_string += "(";
+
+        for p in self.parameters.iter() {}
+
+        return_string += ")\n{\n    ";
+
+        for e in self.body.iter() {
+            return_string += &e.render();
+            return_string += ";\n";
+        }
+
+        return_string += "}";
+
+        return_string
+    }
+}
+
+pub fn test() {
+    let temp: Func = Func {
+        identifier: TypeSpecifier::Void,
+        name: "test_func".to_string(),
+        parameters: vec![],
+        body: vec![Expression {
+            lhs: Box::new(ResolvableValue::Variable("i".to_string())),
+            rhs: Some(RExpression {
+                op: Operator::ArithmeticOperator,
+                rhs: Box::new(ResolvableValue::Value(10)),
+            }),
+        }],
     };
+    println!("{}", temp.render());
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,5 @@
 mod ast;
 
 fn main() {
-    let result = ast::to_postfix("123+2/3".to_string().into_bytes());
-    for t in result {
-        println!("{:?}", t.val);
-    }
+    ast::test();
 }
