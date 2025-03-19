8ecba9de1a4862db475d4cce38b6e2b0fb8a0b66
Joseph Straw <joseph_straw@outlook.com>
2025-03-19 17:04:14 +0000

    Reimplemented type system and lexer for more idiomatic types

--- src/ast.rs
+++ src/ast.rs
@@ -6,42 +6,158 @@ use std::{
     path::Path,
 };
 
-#[repr(u8)]
-#[derive(Clone, Copy)] //TODO remove this trait if possible. Copy is cheap but clone is not
-enum TokenType {
-    Type,
-    Identifier,
-    Keyword,
-    Seperator,
-    Operator,
-    Literal,
-    Comment,
-    Whitespace,
+// type_specifier   : Int
+//                  | Void
+//                  | ...
+//
+// identifier       : [a-z][A-Z][0-9]
+//
+// parameter        : type_specifier identifier
+//
+// parameter_list   : ( parameter* )
+//
+// Operator         : Add
+//                  | Minus
+//                  | ...
+//
+// expression       : identifier
+//                  | identifier operator
+//                  | operator identifier
+//                  | expression operator expression
+//
+// statement        : expression
+//                  | conditional_statement
+//
+// body             : { statement* }
+//
+// func_declaration : type_specifier identifier parameter_list body
+//
+// declaration      : type_specifier identifier assignment_operator expression
+
+//------------------- Basic Types --------------------------
+
+#[derive(PartialEq)]
+enum Type {
+    Int,
+    Void,
 }
 
-struct Token {
-    t_type: TokenType,
-    val: String,
+type Identifier = String;
+
+#[derive(PartialEq)]
+enum Keyword {
+    If,
+    While,
+    //...
+}
+#[derive(PartialEq)]
+enum Seperator {
+    Any, //Temp value to allow a generic seperator
+    CurlyBrace,
+    Brace,
+    SemiColon,
 }
 
-//debug printing function
+#[derive(PartialEq)]
+enum Operator {
+    Any, //Temp value to allow a generic seperator
+    Add,
+    Minus,
+    Divide,
+    Multiply,
+    Comma,
+    Comparitor,
+    Equal,
+    OSB,
+    CSB,
+}
+
+//Some type verification needed
+type Literal = String;
+
+//Some type verification needed
+type Comment = String;
+
+//Some type verification needed
+type Whitespace = String;
+
+//------------------- Compound Types --------------------------
+
+struct Parameter {
+    ts: Type,
+    id: Identifier,
+}
+
+type ParameterList = Vec<Parameter>;
+
+enum Expression {
+    Identifier(Identifier),
+    PostOperator {
+        exp: Box<Expression>,
+        op: Operator,
+    },
+    PreOperator {
+        op: Operator,
+        exp: Box<Expression>,
+    },
+    SubExpression {
+        lhs: Box<Expression>,
+        op: Operator,
+        rhs: Box<Expression>,
+    },
+}
+
+enum Statement {
+    Expression(Expression),
+    //CondtitionalStatement(ConditionalStatement),
+}
+
+#[derive(PartialEq)]
+enum Token {
+    Type(Type),
+    Identifier(Identifier),
+    Keyword(Keyword),
+    Seperator(Seperator),
+    Operator(Operator),
+    Literal(Literal),
+    Comment(Comment),
+    Whitespace(Whitespace),
+}
+
 impl Token {
-    fn to_string(&self) -> String {
-        let mut output = self.val.clone();
+    fn new(mut input_token: Token, input_string: &str) -> Token {
+        let types = vec!["int", "void"];
+        let keywords = vec!["if"];
+        let operators = vec!["+", "-", "=", "==", "[", "]"];
 
-        match self.t_type as u8 {
-            0 => output += " : Type",
-            1 => output += " : Identifier",
-            2 => output += " : Keyword",
-            3 => output += " : Seperator",
-            4 => output += " : Operator",
-            5 => output += " : Literal",
-            6 => output += " : Comment",
-            7 => output += " : Whitespace",
-            _ => (),
+        if types.contains(&input_string) {
+            //TODO implement matching for each case
+            input_token = Token::Type(Type::Int);
+        } else if keywords.contains(&input_string) {
+            input_token = Token::Keyword(Keyword::If)
+        } else if operators.contains(&input_string) {
+            match input_string {
+                "+" => input_token = Token::Operator(Operator::Add),
+                "-" => input_token = Token::Operator(Operator::Minus),
+                "=" => input_token = Token::Operator(Operator::Equal),
+                "==" => input_token = Token::Operator(Operator::Comparitor),
+                "[" => input_token = Token::Operator(Operator::OSB),
+                "]" => input_token = Token::Operator(Operator::CSB),
+                _ => (),
+            }
         }
 
-        output
+        match input_token {
+            Token::Type(x) => Token::Type(x),
+            Token::Identifier(_) => Token::Identifier(input_string.to_string()),
+            Token::Keyword(x) => Token::Keyword(x),
+            Token::Seperator(x) => Token::Seperator(x),
+            Token::Operator(x) => Token::Operator(x),
+            Token::Literal(_) => Token::Literal(input_string.to_string()),
+            Token::Comment(_) => Token::Comment(input_string.to_string()),
+            Token::Whitespace(_) => Token::Whitespace(input_string.to_string()),
+            _ => Token::Whitespace("".to_string()),
+        }
     }
 }
 
@@ -57,147 +173,136 @@ fn string_from_file(filename: impl AsRef<Path>) -> String {
 fn tokenise(input: String) -> Vec<Token> {
     let mut output: Vec<Token> = vec![];
     let mut cur_token_string = "".to_string();
-    let mut cur_token_type = TokenType::Whitespace;
+    let mut cur_token_type: Token = Token::Whitespace("".to_string());
     for c in input.as_bytes() {
         match c {
             //Whitespace
             // ' ' \n
             32 | 10 => {
-                if cur_token_type as u8 != TokenType::Whitespace as u8 {
-                    output.push(Token {
-                        t_type: cur_token_type,
-                        val: cur_token_string,
-                    });
+                if cur_token_type != Token::Whitespace("".to_string()) {
+                    output.push(Token::new(cur_token_type, &cur_token_string));
                 }
                 cur_token_string = "".to_string();
-                cur_token_type = TokenType::Whitespace;
+                cur_token_type = Token::Whitespace("".to_string());
             }
             //Identifier
             //Keyword
             //Seperator
             // { } ( ) ;
             123 | 125 | 40 | 41 | 59 => {
-                if cur_token_type as u8 != TokenType::Whitespace as u8 {
-                    output.push(Token {
-                        t_type: cur_token_type,
-                        val: cur_token_string,
-                    });
+                if cur_token_type != Token::Whitespace("".to_string()) {
+                    output.push(Token::new(cur_token_type, &cur_token_string));
                 }
-                cur_token_type = TokenType::Seperator;
+                cur_token_type = Token::Seperator(Seperator::Any);
                 cur_token_string = (*c as char).to_string();
             }
             //Operator [ ]
             42..=46 | 60..=62 | 91 | 93 => {
-                if cur_token_type as u8 != TokenType::Operator as u8
-                    && cur_token_type as u8 != TokenType::Whitespace as u8
+                if cur_token_type != Token::Operator(Operator::Any)
+                    && cur_token_type != Token::Whitespace("".to_string())
                 {
-                    output.push(Token {
-                        t_type: cur_token_type,
-                        val: cur_token_string,
-                    });
+                    output.push(Token::new(cur_token_type, &cur_token_string));
                     cur_token_string = "".to_string();
                 }
-                cur_token_type = TokenType::Operator;
+                cur_token_type = Token::Operator(Operator::Add);
                 cur_token_string.push(*c as char);
             }
             //Literal
             //Comment
             //Any alphanumeric value or _ "
             47..=57 | 65..=90 | 97..=122 | 95 | 34 => {
-                if cur_token_type as u8 != TokenType::Identifier as u8
-                    && cur_token_type as u8 != TokenType::Whitespace as u8
+                if cur_token_type != Token::Identifier("".to_string())
+                    && cur_token_type != Token::Whitespace("".to_string())
                 {
-                    output.push(Token {
-                        t_type: cur_token_type,
-                        val: cur_token_string,
-                    });
+                    output.push(Token::new(cur_token_type, &cur_token_string));
                     cur_token_string = "".to_string();
                 }
-                cur_token_type = TokenType::Identifier;
+                cur_token_type = Token::Identifier("".to_string());
                 cur_token_string.push(*c as char);
             }
             _ => (), //?
         }
     }
-    output.push(Token {
-        t_type: cur_token_type,
-        val: cur_token_string,
-    });
+    output.push(Token::new(cur_token_type, &cur_token_string));
     output
-}
-
-//Converts any given "Identifier" type to more specific token types based on the string
-fn parse_type(input: &str) -> TokenType {
-    let types = vec!["int", "void"];
-    let keywords = vec!["if"];
-    let operators = vec!["=="];
-
-    //potentially unsafe but if an empty string hits here then other things are wrong
-    if input.chars().next().unwrap() == '"' {
-        return TokenType::Literal;
-    }
-    //TODO unsafe and also not capturing corretctly
-    if input.chars().next().unwrap() == '/' {
-        return TokenType::Comment;
-    }
-
-    //these are safe and fine
-    if input.chars().all(|x| x.is_numeric()) {
-        return TokenType::Literal;
-    }
-    if types.contains(&input) {
-        return TokenType::Type;
-    }
-    if keywords.contains(&input) {
-        return TokenType::Keyword;
-    }
-    if operators.contains(&input) {
-        return TokenType::Operator;
-    }
-
-    return TokenType::Identifier;
 }
 
 pub fn run_lexer(debug: bool) {
     //read file
     let lines = string_from_file("test_code/input.c");
-    //tokenise string
+    //tokenise and lex string
     let mut tokens = tokenise(lines);
-    //update token metadata (identifier) with relevant type. (this is what makes this a lexer)
-    for t in tokens.iter_mut() {
-        if t.t_type as u8 == TokenType::Identifier as u8 {
-            t.t_type = parse_type(&t.val);
-        }
-    }
     //temp debug switch for printing out the current tokens
     if debug {
         for t in tokens.iter_mut() {
             println!("{}", t.to_string());
         }
     }
-    //put into AST
-    let mut mode = "default";
-    let mut current_node: Vec<Token> = vec![];
-    for i in 0..tokens.len() {
-        if mode == "default" {
-            match tokens[i].t_type {
-                TokenType::Type => {
-                    //This should be a reliable way to differentiate this as a function signature
-                    //rather than a declaration but this feels messy
-                    //TODO - fix and re-implement
-                    if tokens[i + 2].val == "(" {
-                        mode = "function sig";
-                    }
-                }
-                TokenType::Identifier => (),
-                TokenType::Keyword => (),
-                TokenType::Seperator => (),
-                TokenType::Operator => (),
-                TokenType::Literal => (),
-                TokenType::Comment => (),
-                TokenType::Whitespace => (),
-            }
-        } else if mode == "function sig" {
+}
+
+//---------------------- Pretty printing functions ---------------------------------
+
+impl Token {
+    fn to_string(&self) -> String {
+        let mut output = "".to_string();
+
+        match self {
+            Token::Type(x) => output += &(x.to_string() + " : Type"),
+            Token::Identifier(x) => output += &(x.to_owned() + " : Identifier"),
+            Token::Keyword(x) => output += &(x.to_string() + " : Keyword"),
+            Token::Seperator(x) => output += &(x.to_string() + " : Seperator"),
+            Token::Operator(x) => output += &(x.to_string() + " : Operator"),
+            Token::Literal(x) => output += &(x.to_owned() + " : Literal"),
+            Token::Comment(x) => output += &(x.to_owned() + " : Comment"),
+            Token::Whitespace(x) => output += &(x.to_owned() + " : Whitespace"),
+        }
+
+        output
+    }
+}
+
+impl Type {
+    fn to_string(&self) -> String {
+        match self {
+            Type::Int => "Int".to_string(),
+            Type::Void => "Void".to_string(),
+        }
+    }
+}
+
+impl Keyword {
+    fn to_string(&self) -> String {
+        match self {
+            Keyword::If => "If".to_string(),
+            Keyword::While => "While".to_string(),
+        }
+    }
+}
+
+impl Seperator {
+    fn to_string(&self) -> String {
+        match self {
+            Seperator::Any => "Any".to_string(),
+            Seperator::CurlyBrace => "CurlyBrace".to_string(),
+            Seperator::Brace => "Brace".to_string(),
+            Seperator::SemiColon => "SemiColon".to_string(),
+        }
+    }
+}
+
+impl Operator {
+    fn to_string(&self) -> String {
+        match self {
+            Operator::Any => "Any".to_string(),
+            Operator::Add => "Add".to_string(),
+            Operator::Minus => "Minus".to_string(),
+            Operator::Divide => "Divide".to_string(),
+            Operator::Multiply => "Multiply".to_string(),
+            Operator::Comma => "Comma".to_string(),
+            Operator::Comparitor => "Comparitor".to_string(),
+            Operator::Equal => "Equal".to_string(),
+            Operator::OSB => "[".to_string(),
+            Operator::CSB => "]".to_string(),
         }
     }
 }
--- src/ast/token_types.rs
+++ src/ast/token_types.rs
@@ -59,33 +59,30 @@
 //
 // return_statement
 //  : return expression
-//
-//
-// Below is valid code but commented out to avoid warnings on compilation
 
 pub enum TypeSpecifier {
     Void,
     Int,
 }
 
-//enum AssignmentOperator {
-//    Equals,
-//}
-//
-//enum ArithmeticOperator {
-//    Plus,
-//    Minus,
-//}
-//
-//enum PrefixOperator {
-//    UnaryPrefixPlus,
-//    UnaryPrefixMinus,
-//}
-//
-//enum PostfixOperator {
-//    UnaryPostfixPlus,
-//    UnaryPostfixMinus,
-//}
+enum AssignmentOperator {
+    Equals,
+}
+
+enum ArithmeticOperator {
+    Plus,
+    Minus,
+}
+
+enum PrefixOperator {
+    UnaryPrefixPlus,
+    UnaryPrefixMinus,
+}
+
+enum PostfixOperator {
+    UnaryPostfixPlus,
+    UnaryPostfixMinus,
+}
 
 pub enum Operator {
     ArithmeticOperator,
@@ -93,18 +90,18 @@ pub enum Operator {
     PostfixOperator,
 }
 
-//pub enum Seperator {
-//    CurlyBraces,
-//    Braces,
-//    SemiColon,
-//}
-//
-//pub enum Keyword {
-//    Type(TypeSpecifier),
-//    Auto,
-//    Break,
-//    // https://en.cppreference.com/w/c/keyword
-//}
+pub enum Seperator {
+    CurlyBraces,
+    Braces,
+    SemiColon,
+}
+
+pub enum Keyword {
+    Type(TypeSpecifier),
+    Auto,
+    Break,
+    // https://en.cppreference.com/w/c/keyword
+}
 
 enum ResolvableValue {
     Value(i32),
@@ -124,78 +121,19 @@ struct RExpression {
     rhs: Box<ResolvableValue>,
 }
 
-impl Expression {
-    fn render(&self) -> String {
-        let mut return_string = String::from("");
-        match &(*self.lhs) {
-            ResolvableValue::Value(x) => return_string += &x.to_string(),
-            ResolvableValue::Variable(x) => return_string += x,
-            ResolvableValue::Expression(x) => return_string += &x.render(),
-        }
-
-        if let Some(rv) = &self.rhs {
-            return_string += match rv.op {
-                Operator::ArithmeticOperator => "+",
-                _ => "another_one",
-            };
-
-            match &(*rv.rhs) {
-                ResolvableValue::Value(x) => return_string += &x.to_string(),
-                ResolvableValue::Variable(x) => return_string += x,
-                ResolvableValue::Expression(x) => return_string += &x.render(),
-            }
-        }
-
-        return_string
-    }
+enum Statement {
+    Exp(Expression),
 }
 
 pub struct Func {
-    identifier: TypeSpecifier,
-    name: String,
+    //storage_class_specifier   TODO implement
+    //function specifier        TODO implement
+    ret_type: TypeSpecifier,
+    identifier: String,
     parameters: Vec<String>,
-    body: Vec<Expression>,
+    body: Vec<Statement>,
 }
 
-impl Func {
-    pub fn new(
-        in_identifier: String,
-        in_name: String,
-        in_parameters: Vec<String>,
-        in_body: Vec<String>,
-    ) -> Func {
-        Func {
-            identifier: TypeSpecifier::Int,
-            name: in_name,
-            parameters: vec![],
-            body: vec![],
-        }
-    }
-    pub fn render(&self) -> String {
-        let mut return_string = String::from("");
-
-        match &self.identifier {
-            TypeSpecifier::Void => return_string.push_str("void"),
-            TypeSpecifier::Int => return_string.push_str("int"),
-        }
-
-        return_string += " ";
-
-        return_string.push_str(&self.name);
-
-        return_string += "(";
-
-        //for p in self.parameters.iter() {}
-
-        return_string += ")\n{\n    ";
-
-        for e in self.body.iter() {
-            return_string += &e.render();
-            return_string += ";\n";
-        }
-
-        return_string += "}";
-
-        return_string
-    }
+pub enum Structure {
+    Func(Func),
 }
